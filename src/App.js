f293e860190c89cdb80ad41f13fd3e2f579e708b
Fayyaz Ahmad <91311395+Fayyaz-fx@users.noreply.github.com>
2022-01-01 02:38:41 +0000

    first commit
    completed the task,

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App'>
+          Hello <strong>Fayyaz ahmad</strong>
+        </h1>
+        <h2>Learning to code with Panacloud</h2>
+        <ul className='ul-list'>
+          <li>
+            <h2>JavaScript</h2>
+          </li>
+          <li>
+            <h2>HTML</h2>
+          </li>
+          <li>
+            <h2>CSS</h2>
+          </li>
+          <li>
+            <h2>REACT</h2>
+          </li>
+        </ul>
+        <ol>
+          <li>
+            <h2>Cloud Native Computing</h2>
+          </li>
+          <li>
+            <h2>Artificial intelligence</h2>
+          </li>
+          <li>
+            <h2>Block chain</h2>
+          </li>
+          <li>
+            <h2>Internet of things</h2>
+          </li>
+        </ol>
+        <p>total : {5 + 10}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
